c6979d9bd4cf1b8238dc68d656f48e9172c50112
Nathan Durst <nathandurst3@gmail.com>
2018-04-25 17:02:07 -0500

    pushing interface/abstract/concrete practice

---
+++ Interfaces-Abstract-Concrete/Polygons/Polygons/Octagon.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons
+{
+    public class Octagon : ConcreteRegularPlygon
+    {
+        public Octagon(int length):
+            base(4, length)
+        { }
+
+        public override double GetArea()
+        {
+            return 2 *(1 + Math.Sqrt(2)) * SideLength * SideLength;
+        }
+    }
+}
---
+++ Interfaces-Abstract-Concrete/Polygons/Polygons/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Polygons
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var square = new Square(5);
+            var triangle = new Triangle(5);
+            DisplayPolygon("Square", square);
+            DisplayPolygon("Triangle", triangle);
+
+            Console.ReadLine();
+
+        }
+
+        public static void DisplayPolygon (string polygonType, dynamic polygon)
+        {
+            try
+            {
+                Console.WriteLine(polygonType + ":");
+                Console.WriteLine("Number of Sides: " + polygon.NumberOfSides);
+                Console.WriteLine("Side Length: " + polygon.SideLength);
+                Console.WriteLine("Perimiter: " + polygon.GetPerimeter());
+                Console.WriteLine("Area: " + polygon.GetArea() + "\n\n");
+                
+
+            }
+            catch(Exception )
+            {
+                Console.WriteLine("Error");
+            }
+        }
+    }
+}
